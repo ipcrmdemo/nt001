02aa6ee2996949b954c262832bb1d3986bdac45b
Matt Cadorette <mcadorette@gmail.com>
2018-10-09 10:32:40 -0400

    Initial commit from Atomist

---
+++ src/main/java/nt001/Nt001Application.java
@@ -0,0 +1,12 @@
+package nt001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nt001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nt001Application.class, args);
+	}
+}
